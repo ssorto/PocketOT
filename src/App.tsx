20cc2ccf933bc21f6db5612bec65decb6da0d439
Othneil Lewis <ol45@Othneils-MacBook-Air.local>
2025-11-08 17:43:00 -0500

    1st commit

--- src/App.tsx
+++ src/App.tsx
@@ -214,6 +214,7 @@ export default function App() {
             />
           )}
 
+// test line
           {clientView === 'summary' && (
             <ClientSummary
               pillars={getSummaryPillars()}
